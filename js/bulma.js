c78cdcd3a614faa42a6bfee069c80822ae2f4c43
Kyle May <kyle@accrubit.com>
2018-12-13 01:59:39 -0700

    Added modals to certs
    Implemented some js, css, and html regarding modal functionality for certification tags.

--- js/bulma.js
+++ js/bulma.js
@@ -1,3 +1,18 @@
+// Modal function for certifications
+var APlus = {
+  apluspdf: {
+    openAPlus:  function() { document.getElementById('APlus').classList.add('is-active');},
+    closeAPlus: function() { document.getElementById('APlus').classList.remove('is-active');}
+  }
+};
+
+var linuxPro = {
+  linuxpdf: {
+    openLinuxPro:  function() { document.getElementById('linuxPro').classList.add('is-active');},
+    closeLinuxPro: function() { document.getElementById('linuxPro').classList.remove('is-active');}
+  }
+};
+
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 (function() {
